61febf40423d6aa305fd9bb8982d515629b08452
maryammf2 <fuentesmaryam2@gmail.com>
2025-11-19 20:52:09 -0600

    Creación de la carpeta Data y de Data.ReservaRepository

---
+++ GestionReservas_LaboratoriosDeComputo/Data/ReservaRepository.cs
@@ -0,0 +1,25 @@
+﻿using GestionReservas_LaboratoriosDeComputo.Models;
+
+namespace GestionReservas_LaboratoriosDeComputo.Data
+{
+    public class ReservaRepository
+    {
+        private static List<Reserva> _reservas = new List<Reserva>();
+
+        public void Agregar(Reserva reserva)
+        {
+            _reservas.Add(reserva);
+        }
+
+        public List<Reserva> ObtenerTodas()
+        {
+            return _reservas;
+        }
+
+        public bool CodigoExiste(string codigo)
+        {
+            return _reservas.Any(r => r.Codigo == codigo);
+        }
+
+    } //class
+} //end
